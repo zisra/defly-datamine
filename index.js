d6a324b940e158ccd8e5a5a4ded2c6ce60a02792
zisra <100528712+zisra@users.noreply.github.com>
2022-04-25 16:29:10 -0500

    Website changes

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-// Error compiling input. Try to check 'Transpile to ES5' in settings:
-
-Line 1: Parse error. Semi-colon expected
+// ERROR! Transpilng failed. Try disabling the transpiling step in the settings.
